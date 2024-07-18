b5d06f04e341498bbb7d82afae1c77a08d246324
qianwei <david.qianwei99@gmail.com>
2024-07-18 19:39:47 +0800

    Modified and completed the admin site

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -3,8 +3,49 @@ from .models import Author, Genre, Book, BookInstance, Language
 
 # Register your models here.
 
-admin.site.register(Book)
-admin.site.register(Author)
+#admin.site.register(Book)
+# admin.site.register(Author)
 admin.site.register(Genre)
-admin.site.register(BookInstance)
+#admin.site.register(BookInstance)
 admin.site.register(Language)
+
+
+class BooksInline(admin.TabularInline):
+    model = Book
+    #extra = 0
+
+@admin.register(Author)
+# Define the admin class
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BooksInline]
+    
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+    
+# Register the Admin classes for Book using the decorator
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    
+    inlines = [BooksInstanceInline]
+
+# Register the Admin classes for BookInstance using the decorator
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'due_back', 'id')
+    
+    list_filter = ('status', 'due_back')
+    
+    fieldsets = (
+        ('Information', {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+    
--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -82,6 +82,12 @@ class Book(models.Model):
     def get_absolute_url(self):
         """Returns the URL to access a detail record for this book."""
         return reverse('book-detail', args=[str(self.id)])
+    
+    def display_genre(self):
+        """Create a string for the Genre. This is required to display genre in Admin."""
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+
+    display_genre.short_description = 'Genre'
 
 class BookInstance(models.Model):
 
